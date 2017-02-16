d0e333486844f852bd502a91c0a81658589b524b
Wuriyanto <wuriyanto48@yahoo.co.id>
2017-02-16 13:54:18 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Node JS Json Web Token using Passport Js for securing api example
+- express js
+- passport js
+- passport jwt
+- jsonwebtoken
+
+*how to get start :
+- go to your project root
+- npm install
+- npm start
---
+++ index.js
@@ -0,0 +1,58 @@
+let express = require('express');
+let path = require('path');
+let favicon = require('serve-favicon');
+let logger = require('morgan');
+let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
+
+let auth = require('./lib/config/auth');
+
+let authHandler = require('./lib/handlers/auth_handler');
+let userHandler = require('./lib/handlers/user_handler');
+
+let app = express();
+
+// view engine setup
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(cookieParser());
+//app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(auth.init());
+
+app.get('/', (req, res, next) => {
+  res.status(200).json({message: 'your app is already to use...'});
+});
+
+app.post('/auth', authHandler);
+
+app.get('/profile', auth.isAuthenticated, userHandler.getProfile);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send({
+    message: err.message,
+    error: err
+    });
+});
+
+module.exports = app;
